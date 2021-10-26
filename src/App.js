559fbe82beee6605d3413b4612de338fd5208b0a
mrBondD1 <hrxbond@gmail.com>
2021-10-26 17:17:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const [ pokemonName, setPokemonName ] = useState('');
+  const [ choosenPokemon, setChoosenPokemon ] = useState(false);
+  const [ pokemonInfo, setPokemonInfo ] = useState({
+        name: "",
+        species:"",
+        img: "",
+        hp:"",
+        attack: "",
+        defence: "",
+        type: "",
+  });
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((res) => {
+      setPokemonInfo({
+        name: pokemonName,
+        species: res.data.species.name,
+        img: res.data.sprites.front_default,
+        hp: res.data.stats[0].base_stat,
+        attack: res.data.stats[1].base_stat,
+        defence: res.data.stats[2].base_stat,
+        type: res.data.types[0].type.name,
+      })
+      setChoosenPokemon(true);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+      <h1>PokiVerse</h1>
+      <input type="text" onChange={(e) => {
+        setPokemonName(e.target.value);
+      }}/>
+      <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div className="displayInfo">
+      {!choosenPokemon ? (
+        <h1>Please choose a valid pokemon!</h1>
+      ) : (
+        <>
+        <h1>{pokemonInfo.name}</h1>
+        <img src={pokemonInfo.img} alt="" />
+        <h3> Species: {pokemonInfo.species}</h3>
+        <h3> Type: {pokemonInfo.type}</h3>
+        <h4> Hp: {pokemonInfo.hp}</h4>
+        <h4> Attack: {pokemonInfo.attack}</h4>
+        <h4> Defense: {pokemonInfo.defence}</h4>
+        </>
+      )
+    }
+      </div>
     </div>
   );
 }
